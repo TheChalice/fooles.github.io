06e749be925a99796336e1816cb4a07a4c582a7b
jiangtong <jt5016224@163.com>
2016-03-21 23:38:48 +0800

    第一章完成

--- javascripts/index.js
+++ javascripts/index.js
@@ -23,3 +23,138 @@ $(".accordion").click(function(e) {
     $(target).css({"border-bottom-right-radius":"0", "border-bottom-left-radius":"0"});
   }
 });
+$(function () {
+    function scroll() {
+        if (window.pageYOffset != null) {
+            //正常浏览器
+            return {
+                top: window.pageYOffset,
+                left: window.pageXOffset
+            }
+        } else if (document.compatMode == "CSS1Compat") {
+            //有DTD的网页
+            return {
+                top: document.documentElement.scrollTop,
+                left: document.documentElement.scrollLeft
+            }
+        } else {
+            //没有DTD的
+            return {
+                top: document.body.scrollTop,
+                left: document.body.scrollLeft
+            }
+        }
+    }
+    function client() {
+        if (window.innerWidth != null) {
+            return {
+                width: window.innerWidth,
+                height: window.innerHeight
+            }
+        } else if (document.compatMode == "CSS1Compat") {
+            return {
+                width: document.documentElement.clientWidth,
+                height: document.documentElement.clientHeight
+            }
+        } else {
+            return {
+                width: document.body.clientWidth,
+                height: document.body.clientHeight
+            }
+        }
+    }
+
+    function getStyle(obj, attr) {
+        if (obj.currentStyle) {
+            return obj.currentStyle[attr];
+        } else {
+            return window.getComputedStyle(obj, null)[attr];
+        }
+    }
+    var sj = $("#phone")[0];
+    var timer2=null
+    var c=0
+    function a(){
+        clearTimeout(timer2)
+        sj.style.backgroundPositionX=c+"px"
+        c=c-80
+        if(c==-1280){
+            c=-720
+        }
+
+        timer2=setTimeout("a()",100)
+    }
+    function b(){
+        clearTimeout(timer2)
+        sj.style.backgroundPositionX=c+"px"
+        c=c+80
+        if(c!=0){
+            timer2=setTimeout("b()",100)
+        }else{
+            c=0
+        }
+
+
+    }
+    var pic0 = $("#pic")[0];
+    var pic1 = $("#pic1")[0];
+    var ren = $("#ren")[0];
+    var ren1 = $("#ren1")[0];
+    var timer1 = null
+    var leaner = 0
+    window.onscroll = function () {
+        if (scroll().top > 0) {
+            pocketmon1(pic0, scroll().top - 3550)
+            pocketmon1(pic1, scroll().top - 3550)
+            ren.style.backgroundPosition = "-33px -167px"
+            ren.style.right = "-45px"
+
+        } else {
+            pocketmon1(pic0, scroll().top - 3900)
+            pocketmon1(pic1, scroll().top - 3900)
+            ren.style.backgroundPosition = "-143px 0px"
+            ren.style.right = "-15px"
+
+        }
+
+        leaner = scroll().top
+    }
+    ren.onmouseover = function () {
+        ren.style.backgroundPosition = "-33px 0px"
+        ren.style.right = "1px"
+    }
+    ren.onmouseout = function () {
+        ren.style.backgroundPosition = "-33px -167px"
+        ren.style.right = "-45px"
+    }
+    ren.onclick = function () {
+
+        clearInterval(timer1)
+        timer1 = setInterval(function () {
+            var target = 0
+            var spet = (target - leaner) / 10
+            spet = spet > 0 ? Math.ceil(spet) : Math.floor(spet);
+            leaner = leaner + spet
+            window.scrollTo(0, leaner)
+            if (leaner == target) {
+                clearInterval(timer1);
+            }
+
+
+        }, 30)
+
+    }
+    function pocketmon1(obj, target) {
+        clearInterval(obj.timer);
+        obj.timer = setInterval(function () {
+            var leader = obj.offsetTop
+            var spet = (target - leader) / 10
+            spet = spet > 0 ? Math.ceil(spet) : Math.floor(spet);
+            obj.style.top = obj.offsetTop + spet + "px"
+            if (leader == target) {
+                clearInterval(obj.timer);
+            }
+        }, 30)
+
+    }
+})
